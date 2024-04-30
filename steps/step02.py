59b886ecacc0e4c04cbe6a9ba4db53218f806719
tamakiii <tamakiii@users.noreply.github.com>
2024-04-30 15:20:30 +0900

    init steps/step02.py

---
+++ steps/step02.py
@@ -0,0 +1,23 @@
+import numpy as np
+from numpy import ndarray
+
+
+class Variable:
+    def __init__(self, data: ndarray):
+        self.data = data
+
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = x**2
+        output = Variable(y)
+        return output
+
+
+x = Variable(np.array(10))
+f = Function()
+y = f(x)
+
+print(type(y))
+print(y.data)
