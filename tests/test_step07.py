bae15c45156b84cfc4ae3248ab5320e9cdca0a23
tamakiii <tamakiii@users.noreply.github.com>
2024-04-30 19:43:54 +0900

    init tests/test_step07.py

---
+++ tests/test_step07.py
@@ -0,0 +1,29 @@
+import pytest
+import numpy as np
+from steps.step07 import Variable, Square, Exp, Square
+
+def test():
+    A = Square()
+    B = Exp()
+    C = Square()
+
+    x = Variable(np.array(0.5))
+    a = A(x)
+    b = B(a)
+    y = C(b)
+
+    y.grad = np.array(1.0)
+
+    C = y.creator
+    b = C.input
+    b.grad = C.backward(y.grad)
+
+    B = b.creator
+    a = B.input
+    a.grad = B.backward(b.grad)
+
+    A = a.creator
+    x = A.input
+    x.grad = A.backward(a.grad)
+
+    np.testing.assert_almost_equal(x.grad, 3.29744, decimal=5)
