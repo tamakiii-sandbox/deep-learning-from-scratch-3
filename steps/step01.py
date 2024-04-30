ecf9e9724cc38448551e14efb50c31cd06ec46b4
tamakiii <tamakiii@users.noreply.github.com>
2024-04-30 13:51:25 +0900

    make format

--- steps/step01.py
+++ steps/step01.py
@@ -1,9 +1,11 @@
 import numpy as np
 from numpy import ndarray
 
+
 class Variable:
     def __init__(self, data: ndarray):
         self.data = data
+
 
 data = np.array(1.0)
 x = Variable(data)
@@ -15,8 +17,10 @@ print("x.ndim:", x.ndim)
 y = np.array([1, 2, 3])
 print("y.ndim:", y.ndim)
 
-z = np.array([
- [1, 2, 3],
- [4, 5, 6],
-])
+z = np.array(
+    [
+        [1, 2, 3],
+        [4, 5, 6],
+    ]
+)
 print("z.ndim:", z.ndim)
