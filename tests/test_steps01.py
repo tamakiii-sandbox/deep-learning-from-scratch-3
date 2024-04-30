2b92a29a86f56022d08d52b3547d6dadc8512c65
tamakiii <tamakiii@users.noreply.github.com>
2024-04-30 14:15:05 +0900

    make format

--- tests/test_steps01.py
+++ tests/test_steps01.py
@@ -2,12 +2,18 @@ import numpy as np
 import pytest
 from step01 import Variable
 
+
 def test_variable_initialization():
     data = np.array(1.0)
     var = Variable(data)
-    assert np.array_equal(var.data, data), "The data should be correctly assigned to var.data"
+    assert np.array_equal(
+        var.data, data
+    ), "The data should be correctly assigned to var.data"
+
 
 def test_variable_data_type():
     data = np.array(1.0)
     var = Variable(data)
-    assert isinstance(var.data, np.ndarray), "var.data should be an instance of numpy.ndarray"
+    assert isinstance(
+        var.data, np.ndarray
+    ), "var.data should be an instance of numpy.ndarray"
