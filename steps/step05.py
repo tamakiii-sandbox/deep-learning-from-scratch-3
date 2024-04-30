6696f5b1973a62b01dea7b22304523a6a19a19bc
tamakiii <tamakiii@users.noreply.github.com>
2024-04-30 19:01:00 +0900

    init step05.py

---
+++ steps/step05.py
@@ -0,0 +1,63 @@
+import numpy as np
+from numpy import ndarray
+
+
+class Variable:
+    def __init__(self, data: ndarray):
+        self.data = data
+        self.grad = None # 勾配（Gradient）
+
+class Function:
+    def __call__(self, input: Variable):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        self.input = input
+        return output
+
+    def forward(self, x: float):
+        raise NotImplementedError()
+
+    def backward(self, gy: float):
+        raise NotImplementedError()
+
+class Square(Function):
+    def forward(self, x: float):
+        y = x ** 2
+        return y
+
+    def backward(self, gy: float):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
+class Exp(Function):
+    def forward(self, x: float):
+        y = np.exp(x)
+        return y
+
+    def backward(self, gy: float):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+
+def main():
+    A = Square()
+    B = Exp()
+    C = Square()
+
+    x = Variable(np.array(0.5))
+    a = A(x)
+    b = B(a)
+    y = C(b)
+
+    y.grad = np.array(1.0)
+    b.grad = C.backward(y.grad)
+    a.grad = B.backward(b.grad)
+    x.grad = A.backward(a.grad)
+
+    print(y.data)
+    print(x.grad)
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_step05.py
@@ -0,0 +1,22 @@
+import pytest
+import numpy as np
+from steps.step05 import Square, Exp, Variable
+
+def test():
+    A = Square()
+    B = Exp()
+    C = Square()
+
+    x = Variable(np.array(0.5))
+    a = A(x)
+    b = B(a)
+    y = C(b)
+
+    y.grad = np.array(1.0)
+    b.grad = C.backward(y.grad)
+    a.grad = B.backward(b.grad)
+    x.grad = A.backward(a.grad)
+
+    np.testing.assert_almost_equal(y.data, 1.64872, decimal=5)
+    np.testing.assert_almost_equal(x.grad, 3.29744, decimal=5)
+
