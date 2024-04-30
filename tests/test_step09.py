7668c8de6cfc02290dbf276866bbedd7bd152d52
tamakiii <tamakiii@users.noreply.github.com>
2024-04-30 20:32:10 +0900

    treat case grad is None

--- tests/test_step09.py
+++ tests/test_step09.py
@@ -9,4 +9,13 @@ def test():
 
     y.grad = np.array(1.0)
     y.backward()
+
+    np.testing.assert_almost_equal(x.grad, 3.29744, decimal=5)
+
+
+def test_only_backward():
+    x = Variable(np.array(0.5))
+    y = square(exp(square(x)))
+    y.backward()
+
     np.testing.assert_almost_equal(x.grad, 3.29744, decimal=5)
